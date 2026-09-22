bfef0290707a42d3fa12c3979bb93e7b108fad35
Kalyan123-cell <gogukalyan1998@gmail.com>
2026-09-22 15:52:58 +0530

    Fix syntax errors in print statements

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-print("hello, this is created to test Jenkins"
+print("hello, this is created to test Jenkins")
+echo("This is to enhance the")
 
 
-print("this is to close the jenkins test"
+print("this is to close the jenkins test")
